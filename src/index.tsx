20bb9eef4d330a9570d728a675b3beb67d9a0699
Khoi Hoang <khoii87@gmail.com>
2021-08-24 15:31:56 +0200

    Fix: 📦 sass-loader
    Fixed by uninstall current sass-loader, then this:
    
    1. npm install sass-loader@latest
    1. npm install sass

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalContextProvider from './store/GlobalContextProvider';
+import './index.scss';
 
 ReactDOM.render(
   <GlobalContextProvider>
